b3cd3fcf23435a2e616985f21d7757c1b25973c9
itl_store <462799794@qq.com>
2020-08-18 14:10:15 +0800

    去掉不必要的代码

--- imperial_parent/imperial_work/src/main/java/com/project/app/imperial/controller/ImpBasicDataController.java
+++ imperial_parent/imperial_work/src/main/java/com/project/app/imperial/controller/ImpBasicDataController.java
@@ -1,12 +1,9 @@
 package com.project.app.imperial.controller;
 
-import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
-import javax.servlet.http.HttpServletRequest;
 
-import org.apache.shiro.SecurityUtils;
 import org.apache.shiro.authz.annotation.RequiresPermissions;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
